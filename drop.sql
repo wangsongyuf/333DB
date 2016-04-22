297f4376fde62c0ee9b99f423338a3224a5e9635
wangsongyuf <wangs4@rose-hulman.edu>
2016-04-21 21:17:36 -0400

    a

---
+++ drop.sql
@@ -0,0 +1,11 @@
+use Graduate_School
+go
+drop table WorkIn
+drop table FRate
+drop table CRate
+drop table ThisUser
+drop table Supervise
+drop table Faculty
+drop table Program
+drop table Department
+drop table College
---
+++ print demo data.sql
@@ -0,0 +1,21 @@
+use Graduate_School
+go
+select * 
+from College
+
+select * 
+from CRate
+select * 
+from Department
+select * 
+from Faculty
+select * 
+from FRate
+select * 
+from Program
+select * 
+from Supervise
+select * 
+from ThisUser
+select * 
+from WorkIn
