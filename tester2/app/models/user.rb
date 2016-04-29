25583a326c16d80cfe1de9361091fdf8458a5963
wangsongyuf <wangs4@rose-hulman.edu>
2016-04-29 14:03:10 -0400

    a

--- tester2/app/models/user.rb
+++ tester2/app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
   include ActiveModel::Validations
   validates_presence_of :first_name, :last_name
   validates_length_of :password,
-   :minimum => 8
+   :minimum => 8    ,       # more than 8 characters
+   :maximum => 16    ,      # shorter than 16 characters
+   :in => 8..16            # between 8 and 16 characters
    
    validates_uniqueness_of :username
    validates_format_of :first_name,
