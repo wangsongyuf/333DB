318f4e17626c935c8f169681d9464b050c5b42d0
wangsongyuf <wangs4@rose-hulman.edu>
2016-04-29 14:01:12 -0400

    more invaild

--- tester2/app/models/user.rb
+++ tester2/app/models/user.rb
@@ -3,8 +3,13 @@ class User < ActiveRecord::Base
   include ActiveModel::Validations
   validates_presence_of :first_name, :last_name
   validates_length_of :password,
-   :minimum => 8           # more than 8 characters
+   :minimum => 8
+   
    validates_uniqueness_of :username
+   validates_format_of :first_name,
+      :with => /[a-zA-Z]/
+       validates_format_of :last_name,
+      :with => /[a-zA-Z]/
   has_secure_password
   
 end
