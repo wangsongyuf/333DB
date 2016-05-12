ffd45f880cbbb5019fb07ae414a46e86a313e8cb
wangsongyuf <wangs4@rose-hulman.edu>
2016-05-12 19:17:34 -0400

    change sp, need guy fix

--- tester2/app/controllers/starting_controller.rb
+++ tester2/app/controllers/starting_controller.rb
@@ -7,28 +7,32 @@ class StartingController < ApplicationController
   def start
     if params[:commit] == "Search"
       @connection = ActiveRecord::Base.connection
-      @st='exec search '
-      if params[:starting][:College].empty?
-        @st=@st+'@college = null,'
-      else
-        @st=@st+'@college = '+'\'"'+params[:starting][:College]+'"\''+','
+      if !params[:starting][:College].empty?
+        @st1='exec searchcollege '+'@college ='+'\'"'+params[:starting][:College]+'"\''
+          @result1 = @connection.exec_query(@st1)
+      else @result1
       end
-        if params[:starting][:Department].empty?
-        @st=@st+'@department = null,'
-      else
-        @st=@st+'@department = '+'\'"'+params[:starting][:Department]+'"\''+','
+        if !params[:starting][:Department].empty?
+        @st2='exec searchdepartment '+'@department = '+'\'"'+params[:starting][:Department]+'"\''
+        @result2 = @connection.exec_query(@st2)
+        else @result2
       end
-      if params[:starting][:Program].empty?
-        @st=@st+'@program = null,'
-      else
-        @st=@st+'@program = '+'\'"'+params[:starting][:Program]+'"\''+','
+      if !params[:starting][:Program].empty?
+        @st3='exec searchprogram '+'@program = '+'\'"'+params[:starting][:Program]+'"\''
+        @result3 = @connection.exec_query(@st3)
+      else @result3
       end
-      if params[:starting][:Faculty].empty?
-        @st=@st+'@faculty = null'
-      else
-        @st=@st+'@faculty = '+'\'"'+params[:starting][:Faculty]+'"\''
+      if !params[:starting][:Faculty].empty?
+        @st4='exec searchfaculty '+'@faculty = '+'\'"'+params[:starting][:Faculty]+'"\''
+        @result4 = @connection.exec_query(@st4)
+      else @result4
       end
-      @result = @connection.exec_query(@st)
+      
+      puts @result1
+      puts @result2
+      puts @result3
+      puts @result4
+      @result=@result1.merge(@result2).merge(@result3).merge(@result4)
       
       count = 0
       @result.each do |p|
