2ed44d5ac3dc4a8b9cd9f84ea43b7ca91c729200
KaetheKong <kaethekung@gmail.com>
2016-05-12 20:33:13 -0400

    minor changes
    as desp

--- tester2/app/controllers/starting_controller.rb
+++ tester2/app/controllers/starting_controller.rb
@@ -11,22 +11,22 @@ class StartingController < ApplicationController
       if !params[:starting][:College].empty?
         @st1='exec searchcollege '+'@college ='+'\'"'+params[:starting][:College]+'"\''
         @result1 = @connection.exec_query(@st1)
-      else @result1
+      else @result1 = {}
       end
       if !params[:starting][:Department].empty?
         @st2='exec searchdepartment '+'@department = '+'\'"'+params[:starting][:Department]+'"\''
         @result2 = @connection.exec_query(@st2)
-      else @result2
+      else @result2 = {}
       end
       if !params[:starting][:Program].empty?
         @st3='exec searchprogram '+'@program = '+'\'"'+params[:starting][:Program]+'"\''
         @result3 = @connection.exec_query(@st3)
-      else @result3
+      else @result3 = {}
       end
       if !params[:starting][:Faculty].empty?
         @st4='exec searchfaculty '+'@faculty = '+'\'"'+params[:starting][:Faculty]+'"\''
         @result4 = @connection.exec_query(@st4)
-      else @result4
+      else @result4 = {}
       end
 
       render :results
